a1f3013d18a8c0e8554d3572cc5042bd346241d5
Dan Romans <danieljromans@gmail.com>
2019-08-08 23:53:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,57 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do 
+    new.push(yield(source_array[i]))
+    i += 1
+    end
+  return new
+end
+
+
+#def reduce(source_array, starting_point = 0)
+ # value = starting_point
+  #source_array.each do |i|
+   # value = yield(value, i)
+#  end
+#  return value                          
+#end
+
+
+def reduce(source_array, starting_point = 0)
+  value = source_array[0]
+  i = 0
+  while i < source_array.length do
+    value = yield(value, source_array[i])
+    i += 1
+  end
+  return value
+end
+  
+
+# source_array = [ false, nil, nil, nil, true]
+# {|memo, n| memo || n}
+
+
+=begin
+def reduce_to_total(array, initializer = 0)
+  value = 0
+  array.each do |i|
+    value += i
+  end
+  return value + initializer
+end
+=end
+
+=begin
+def reduce_to_any_true(array)
+  i = 0 
+  while i < array.length do
+    if array[i] == true 
+      return true
+    end
+    i += 1
+  end
+  return false
+end
+=end
