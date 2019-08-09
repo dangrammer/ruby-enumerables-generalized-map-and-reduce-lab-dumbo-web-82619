02f754514b1bedb485c0acb3baee0487b3e292a0
Dan Romans <danieljromans@gmail.com>
2019-08-09 00:00:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,7 @@ end
 
 
 def reduce(source_array, starting_point = 0)
-  source_array[].class == integer || float ? value = 0 : value = source_array[0]
+  source_array[ ].class == integer || float ? value = 0 : value = source_array[0]
   i = 0
   while i < source_array.length do
     value = yield(value, source_array[i])
