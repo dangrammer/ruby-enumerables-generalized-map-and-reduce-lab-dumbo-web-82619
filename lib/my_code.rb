e38cf4224487d251ab5725767ed7a752a3a62242
Dan Romans <danieljromans@gmail.com>
2019-08-09 00:17:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,31 +27,4 @@ def reduce(source_array, starting_point = 0)
   end
   return value
 end
- =end 
-
-# source_array = [ false, nil, nil, nil, true]
-# {|memo, n| memo || n}
-
-
-=begin
-def reduce_to_total(array, initializer = 0)
-  value = 0
-  array.each do |i|
-    value += i
-  end
-  return value + initializer
-end
-=end
-
-=begin
-def reduce_to_any_true(array)
-  i = 0 
-  while i < array.length do
-    if array[i] == true 
-      return true
-    end
-    i += 1
-  end
-  return false
-end
-=end
+=end 
